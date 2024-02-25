627ae9224b40c50b8414e4a2f650ac833ef0673f
akshay1a <146935480+akshay-1a@users.noreply.github.com>
2024-02-26 00:06:58 +0530

    home responsiveness updated

--- src/app/page.js
+++ src/app/page.js
@@ -6,21 +6,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-96 flex-col items-center justify-between px-24">
+    <main className="relative flex min-h-screen md:min-h-1 flex-col items-center justify-between px-24 md:py-8">
       {/* <div className="backdrop-blur-md absolute inset-0" /> */}
-      <div className="flex w-full justify-between items-center z-10">
+      <div className="flex flex-col md:flex-row w-full mt-20 md:mt-0 gap-20 justify-center md:justify-between items-center z-10">
         {/* <MovingBorder duration={1} rx="30%" ry="30%">
           <div className="bg-gray-600/50 uppercase font-bold rounded-3xl w-[750px] p-10 text-white text-8xl">
             developer KHandelwal
           </div>
         </MovingBorder> */}
         <div
-          className="bg-gray-600/50 uppercase font-bold
-            rounded-3xl w-[750px] text-white text-8xl"
+          className=" uppercase font-bold
+            rounded-3xl  md:w-[750px] text-white text-8xl"
         >
           <Button
             borderRadius="1rem"
-            className="uppercase text-8xl p-10 flex flex-col"
+            className="uppercase md:p-10 flex flex-col hollow-text"
           >
             <TypewriterEffectSmooth
               words={[
@@ -31,7 +31,7 @@ export default function Home() {
                   text: "Carnival",
                 },
               ]}
-              className="text-8xl"
+              className=" text-3xl md:text-8xl"
             />
             {/* <TypewriterEffectSmooth
               words={[
@@ -43,8 +43,8 @@ export default function Home() {
             /> */}
           </Button>
         </div>
-        <CardContainer className="inter-var relative w-96 h-96 bg-cover">
-          <CardBody className="bg-gray-700/40 rounded-3xl">
+        <CardContainer className="inter-var relative w-64 h-64 md:w-80 md:h-80 bg-cover">
+          <CardBody className="bg-gray-700/40 h-full rounded-3xl">
             <CardItem
               translateZ={20}
               className="absolute w-full h-full
@@ -55,20 +55,12 @@ export default function Home() {
               <Image
                 src="/ssiu.png"
                 alt="Me standing on a cliff with"
-                height={600}
-                width={600}
-                className="flex justify-center text-center z-20
-              hover:animate-pulse items-center text-7xl font-bold italic w-full h-full"
+                height={800}
+                width={800}
+                className="flex justify-center text-center z-20 hover:animate-pulse items-center 
+                text-7xl font-bold italic w-full h-full"
               />
             </CardItem>
-            {/* <Image
-            src="/circle.png"
-            alt="Me standing on a cliff with"
-            height={600}
-            width={600}
-            className="absolute animate-spin spin w-full h-full"
-          /> */}
-            {/* <div>GDSC<br/>SSIU</div> */}
           </CardBody>
         </CardContainer>
       </div>
